06c5eba81e7c748e0a89576ad1f4271de24dd910
RSN621 <3400223562@qq.com>
2023-03-20 21:03:53 +0800

    init

--- src/main.js
+++ src/main.js
@@ -1,15 +1,19 @@
 import { createApp } from 'vue'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import axios from './plugins/axios.js'
 import VueAxios from 'vue-cli-plugin-axios'
+import axios from './plugins/axios'
 
 
 
 const app=createApp(App)
-//app.mount('#app')
-app.use(store).use(VueAxios,axios).use(router).mount('#app')
+app.use(VueAxios,axios)
+app
+    .use(store)
+    .use(router)
+    .use(ElementPlus)
+    .mount('#app')
 app.config.globalProperties.$axios=axios
-//createApp(App).use(VueAxios,axios)
-//createApp(App).use(store).use(router).mount('#app')
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,32 @@
 const { defineConfig } = require('@vue/cli-service')
+const AutoImport = require('unplugin-auto-import/webpack')
+const Components = require('unplugin-vue-components/webpack')
+const { ElementPlusResolver } = require('unplugin-vue-components/resolvers')
+
+
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer:{
+    proxy: {
+      '/api':{
+        target: 'http://localhost:6060/', //对应自己的接口
+        changeOrigin: true,
+        ws: true,
+        pathRewrite: {
+          '^/api': ''      //这里理解成用‘/api’代替target里面的地址，后面组件中我们掉接口时直接用api代替
+          // 比如我要调用'http://localhost:8024/management/user/add'，直接写‘/api/user/add’即可
+        }
+      }
+    }
+  }
+},{
+  plugins: [
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+    }),
+  ],
 })
+
